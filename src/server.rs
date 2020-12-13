8e39dd345c44576b7e4f41a3bb1c43c064b0d8e7
Koen Klinkers <k.klinkers@gmail.com>
2020-12-13 18:20:10 +0100

    working

--- src/server.rs
+++ src/server.rs
@@ -3,14 +3,22 @@ use recipe::sourdough;
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 async fn recipe(req: HttpRequest) -> impl Responder {
-    let flower = req.match_info().get("name").unwrap_or("1000").parse::<i32>().expect("invalid value");
-    sourdough(flower,70,20,2,5)
+    let flower = req.match_info().get("flower").unwrap_or("1000").parse::<i32>().unwrap_or(1000);
+    let hydration = req.match_info().get("hydration").unwrap_or("70").parse::<i32>().unwrap_or(70);
+    let starter = req.match_info().get("starter").unwrap_or("20").parse::<i32>().unwrap_or(20);
+    let salt = req.match_info().get("salt").unwrap_or("2").parse::<i32>().unwrap_or(2);
+    let brine_water = req.match_info().get("brine_water").unwrap_or("5").parse::<i32>().unwrap_or(5);
+    sourdough(flower,hydration,starter,salt,brine_water)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .route("/{flower}-{hydration}-{starter}-{salt}-{brine_water}", web::get().to(recipe))
+            .route("/{flower}-{hydration}-{starter}-{salt}", web::get().to(recipe))
+            .route("/{flower}-{hydration}-{starter}", web::get().to(recipe))
+            .route("/{flower}-{hydration}", web::get().to(recipe))
             .route("/{flower}", web::get().to(recipe))
             .route("/", web::get().to(recipe))
     })
