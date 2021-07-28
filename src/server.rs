a38699f78dd424337e6955c83e9d7fe96da5b720
Koen Klinkers <k.klinkers@gmail.com>
2021-07-28 22:18:37 +0200

    rust fmt

--- src/server.rs
+++ src/server.rs
@@ -1,17 +1,42 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use figlet_rs::FIGfont;
 
 mod recipe;
 use recipe::sourdough;
 
 async fn recipe(req: HttpRequest) -> impl Responder {
-    let flower = req.match_info().get("flower").unwrap_or("1000").parse::<i32>().unwrap_or(1000);
-    let hydration = req.match_info().get("hydration").unwrap_or("70").parse::<i32>().unwrap_or(70);
-    let starter = req.match_info().get("starter").unwrap_or("20").parse::<i32>().unwrap_or(20);
-    let salt = req.match_info().get("salt").unwrap_or("2").parse::<i32>().unwrap_or(2);
-    let brine_water = req.match_info().get("brine_water").unwrap_or("5").parse::<i32>().unwrap_or(5);
-    sourdough(flower,hydration,starter,salt,brine_water)
+    let flower = req
+        .match_info()
+        .get("flower")
+        .unwrap_or("1000")
+        .parse::<i32>()
+        .unwrap_or(1000);
+    let hydration = req
+        .match_info()
+        .get("hydration")
+        .unwrap_or("70")
+        .parse::<i32>()
+        .unwrap_or(70);
+    let starter = req
+        .match_info()
+        .get("starter")
+        .unwrap_or("20")
+        .parse::<i32>()
+        .unwrap_or(20);
+    let salt = req
+        .match_info()
+        .get("salt")
+        .unwrap_or("2")
+        .parse::<i32>()
+        .unwrap_or(2);
+    let brine_water = req
+        .match_info()
+        .get("brine_water")
+        .unwrap_or("5")
+        .parse::<i32>()
+        .unwrap_or(5);
+    sourdough(flower, hydration, starter, salt, brine_water)
 }
 
 #[actix_web::main]
@@ -29,15 +54,20 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
-            .route("/{flower}-{hydration}-{starter}-{salt}-{brine_water}", web::get().to(recipe))
-            .route("/{flower}-{hydration}-{starter}-{salt}", web::get().to(recipe))
+            .route(
+                "/{flower}-{hydration}-{starter}-{salt}-{brine_water}",
+                web::get().to(recipe),
+            )
+            .route(
+                "/{flower}-{hydration}-{starter}-{salt}",
+                web::get().to(recipe),
+            )
             .route("/{flower}-{hydration}-{starter}", web::get().to(recipe))
             .route("/{flower}-{hydration}", web::get().to(recipe))
             .route("/{flower}", web::get().to(recipe))
             .route("/", web::get().to(recipe))
     })
-    .bind(format!("{}:{}", url,port))?
+    .bind(format!("{}:{}", url, port))?
     .run()
     .await
 }
-
